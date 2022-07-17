4ba1727635d82a47fd2f0cfd5e1e430ff3637540
Utishnik <yarik.nofamily.00@bk.ru>
2022-07-17 16:20:53 +0300

    Добавьте файлы проекта.

---
+++ Platformer/Platformer.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define MapWeight 120
+#define MapHeight 50
+
+char Map[MapHeight][MapWeight+1];
+
+void Init() {
+	for (int i = 0;i < MapWeight;i++)
+		Map[0][i] = '#';
+	Map[0][MapWeight] = '\0';
+
+	for (int i = 1;i < MapHeight;i++) {
+		sprintf(Map[i], Map[0]);
+	}
+}
+
+void Show() {
+	for (int i = 0;i < MapHeight;i++)
+		printf("%s\n", Map[i]);
+}
+
+int main()
+{
+	Init();
+	Show();
+}
+
